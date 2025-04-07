128d49c1a50a897084417ff57dacc5e802c34532
Yash <yashdesh095@gmail.com>
2025-04-07 09:59:14 +0530

    disabled Eslint

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['lh3.googleusercontent.com',"res.cloudinary.com"],
   },
